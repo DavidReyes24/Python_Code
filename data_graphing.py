ddf0cd1938a0d75f0b9de8289580e2ce54b09732
David Reyes <davidreyesaguilar24@gmail.com>
2025-10-27 03:22:30 -0400

    Made a scatter plot

--- data_graphing.py
+++ data_graphing.py
@@ -1,5 +1,14 @@
 from excel_utils import load_excel
+import pandas as pd
+import matplotlib.pyplot as plt
 
 df = load_excel()
 print(df.head())
+print(df.columns)
 print(df.describe())
+
+plt.scatter(df["Weight_g"], df["Protein_Level"], marker="s", c="#075C4B")
+plt.xlabel("Weight (g)")
+plt.ylabel("Protein_level (arbitrary)")
+plt.title("Protein level vs Weight")
+plt.show()
